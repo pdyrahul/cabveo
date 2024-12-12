5c5dc06b0509faa5e5a7b0a53bd88453c1761ae4
unknown <rahul@gmail.com>
2024-12-12 21:47:20 +0530

    build

--- src/Pages/VerifyPage.jsx
+++ src/Pages/VerifyPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Logo from "../assets/logo.png"
+import Logo from "../assets/Logo.png"
 import verifyPic from '../assets/verifyImg.png'
 import { Link } from 'react-router-dom';
 import { RxCaretLeft } from "react-icons/rx";
