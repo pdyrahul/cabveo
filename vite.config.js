0ea08a6e7e3cfad7ce1fd63dde9c455f5b8a6b6e
unknown <rahul@gmail.com>
2024-12-15 23:48:46 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://veocab-project.vercel.app',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            },
-        },
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://veocab-project.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
     },
+  },
+  build: {
+    outDir: 'build',
+    chunkSizeWarningLimit: 500,
+  },
 });
